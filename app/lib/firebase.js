0a1de53a36d007f3f87e79b3018deecd050692d4
AhmadRaoof Wehbi <ahmadraoofweh@gmail.com>
2026-05-25 15:00:41 +0300

    Reverted

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,31 +1,19 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
-let app;
-let db;
-let auth;
-let storage;
-export function getFirebase() {
-    if (typeof window === "undefined") return null;
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
-    if (!app) {
-        const { initializeApp, getApps } = require("firebase/app");
-        const { getFirestore } = require("firebase/firestore");
-        const { getAuth } = require("firebase/auth");
-        const { getStorage } = require("firebase/storage");
-        const firebaseConfig = {
-            apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-            authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-            storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-            appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
-        };
-
-        app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
-        db = getFirestore(app);
-        auth = getAuth(app);
-        storage = getStorage(app);
-    }
-
-    return { db, auth, storage };
-}
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
