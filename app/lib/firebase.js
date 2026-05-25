d3abd05119766ea6056164e02cf42db60fa38138
AhmadRaoof Wehbi <ahmadraoofweh@gmail.com>
2026-05-25 14:55:30 +0300

    Trying firebase fix (3)

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,7 +1,8 @@
+
 let app;
 let db;
 let auth;
-
+let storage;
 export function getFirebase() {
     if (typeof window === "undefined") return null;
 
@@ -9,17 +10,22 @@ export function getFirebase() {
         const { initializeApp, getApps } = require("firebase/app");
         const { getFirestore } = require("firebase/firestore");
         const { getAuth } = require("firebase/auth");
-
+        const { getStorage } = require("firebase/storage");
         const firebaseConfig = {
             apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
             authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
             projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+
         };
 
         app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
         db = getFirestore(app);
         auth = getAuth(app);
+        storage = getStorage(app);
     }
 
-    return { db, auth };
+    return { db, auth, storage };
 }
